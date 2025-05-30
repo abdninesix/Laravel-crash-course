61382f74a17ff6dbdf7caecd9b11623ada0aac50
abdninesix <mohabd1995@gmail.com>
2025-05-30 15:19:04 +0500

    Updated

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Layout = ({children}) => {
     return (
-        <>
+        <div className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-40 space-y-2 duration-150'>
             <header>
                 <nav className='p-2 w-full flex justify-center gap-5 bg-slate-500 text-white'>
                     <a href='/'>Home</a>
@@ -18,7 +18,7 @@ const Layout = ({children}) => {
             <footer>
                 footer
             </footer>
-        </>
+        </div>
     )
 }
 
--- resources/js/Pages/About/About.jsx
+++ resources/js/Pages/About/About.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Layout from '../../Layouts/Layout'
 
 const About = () => {
   return (
     <div>About</div>
   )
 }
+
+About.layout = page => <Layout children={page} />
 
 export default About
