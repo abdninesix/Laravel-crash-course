9ef2f9f6ab2d93743e789ba11748fde25f702c36
abdninesix <mohabd1995@gmail.com>
2025-05-30 12:17:16 +0500

    Updated

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return inertia('Home');
+    return Inertia::render('Home');
+});
+
+Route::get('/about', function () {
+    return Inertia::render('About/About');
 });
